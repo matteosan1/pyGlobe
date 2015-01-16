c2ddd415da2aca8878b33e4f1306e54f74f3ce58
Andre Holzner <andre.holzner@gmail.com>
2015-01-16 14:41:02 +0100

    now producing interpolating functions in fitSignal.py

--- fitSignal.py
+++ fitSignal.py
@@ -51,7 +51,7 @@ def getGaussianVars(ws, varname, proc, mass, catname):
     retval = []
 
     for gaussIndex in itertools.count():
-        name = utils.makeGaussianVarname("sigma",
+        name = utils.makeGaussianVarname(varname,
                                          proc,
                                          mass,
                                          catname,
@@ -94,6 +94,12 @@ for cat in allCats:
         frame.SetTitle("%s %s" % (cat, proc))
         gcs.append(ROOT.TCanvas())
 
+        # fitted values for this category and signal process
+        # first index is the Gaussian component number
+        # second index is the mass point index
+        sigmaValues = []
+        dmuValues = []
+
         for mass in allMasses:
 
             # get the signal MC dataset
@@ -120,19 +126,61 @@ for cat in allCats:
 
 
             #----------
-            # fix the fitted parameters
+            # fix the fitted parameters and read the fitted values
             #----------
 
             sigmaVars = getGaussianVars(ws, "sigma", proc, mass, cat)
             dmuVars   = getGaussianVars(ws, "dmu", proc, mass, cat)
 
-            for var in sigmaVars + dmuVars:
-                var.setConstant(True)
+            numGaussians = len(sigmaVars)
+            assert numGaussians == len(dmuVars)
+
+            # TODO: sort the Gaussian components, e.g. according to the width
+
+            for vars, values in ((sigmaVars, sigmaValues),
+                                 (dmuVars, dmuValues)):
+
+                if len(values) == 0:
+                    values.extend([[ ] for i in range(numGaussians) ] )
+
+                for gaussIndex, var in enumerate(vars):
+                    var.setConstant(True)
+                    values[gaussIndex].append(var.getVal())
 
             #----------
 
+
         # end of loop over masses
         frame.Draw()
+
+        #----------
+        # produce the interpolating objects
+        #----------
+        for varname, values in (("sigma", sigmaValues),
+                              ("dmu", dmuValues)):
+
+            for gaussIndex in range(len(values)):
+                funcname = utils.makeGaussianVarname("interp_" + varname,
+                                          proc,
+                                          None, # mhyp
+                                          cat,
+                                          gaussIndex
+                                          )
+
+                print "ZZ",values[gaussIndex]
+                func = utils.makePiecewiseLinearFunction(funcname,
+                                                         massVar,
+                                                         allMasses,
+                                                         values[gaussIndex])
+
+                # import this function into the workspace
+                getattr(ws, 'import')(func, ROOT.RooFit.RecycleConflictNodes())
+            # end of loop over Gaussian components
+
+        # end of loop over variables
+
+        # TODO: build the interpolated signal PDF
+
 
     # end of loop over signal processes
 
