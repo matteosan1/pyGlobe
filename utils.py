6a7be1b9fe4b1c9ea8a27cee295dc549e1ea4a04
Andre Holzner <andre.holzner@gmail.com>
2015-01-16 14:40:28 +0100

    added a function makePiecewiseLinearFunction(..) to utils.py

--- utils.py
+++ utils.py
@@ -29,7 +29,59 @@ def makeGaussianVarname(varname, proc, mhyp, catname, gaussIndex):
     return "_".join(parts)
     
 
+#----------------------------------------------------------------------
 
+def makePiecewiseLinearFunction(funcName, xvar, xvalues, yvalues):
+    # creates a RooFormulaVar with an expression for a
+    # piecewise linear function
+
+    numPoints = len(xvalues)
+
+    assert numPoints == len(yvalues)
+
+    assert numPoints >= 2
+
+    # sort indices such that xvlaues are sorted in ascending order
+    indices = range(numPoints)
+    indices.sort(key = lambda index: xvalues[index])
+
+    parts = []
+
+    # below the leftmost point, the function keeps
+    # the value of the leftmost point
+    parts.append("(@0 < %f) * %f" % (xvalues[indices[0]],
+                                     yvalues[indices[0]]))
+
+    for pos in range(1, numPoints):
+        xleft = xvalues[indices[pos-1]]
+        yleft = yvalues[indices[pos-1]]
+
+        xright = xvalues[indices[pos]]
+        yright = yvalues[indices[pos]]
+
+        slope = (yright - yleft) / float(xright - xleft)
+
+        condition = "(@0 >= %f && @0 < %f)" % (xleft, xright)
+        
+        parts.append("%s * (%f + (@0 - %f) * %f)" % (
+            condition,
+            yleft,
+            xleft,
+            slope))
+
+    # beyond the rightmost point, the function again keeps
+    # the value of the rightmost point
     
+    parts.append("(@0 >= %f) * %f" % (xvalues[indices[-1]],
+                                      yvalues[indices[-1]]))
+
+
+    formula = " + ".join(parts)
+
+    import ROOT
+    return ROOT.RooFormulaVar(funcName,funcName,
+                              formula,
+                              ROOT.RooArgList(xvar))
+
 
 #----------------------------------------------------------------------
