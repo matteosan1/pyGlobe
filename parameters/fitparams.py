0c49d07f697692b30cb684c804301b615fb76c91
Andre Holzner <andre.holzner@gmail.com>
2015-01-16 16:15:33 +0100

    initial commit: example fit parameters file

---
+++ parameters/fitparams.py
@@ -0,0 +1,55 @@
+params = {
+
+    #----------
+    "cat5": {
+    #----------
+        "vbf": {
+            125: dict(
+                sigma0_min = 1,
+                sigma1_min = 1,
+                ),
+
+            140: dict(
+                sigma0_min = 1,
+                sigma1_min = 1,
+                ),
+
+            150: dict(
+                sigma0_min = 1,
+                sigma1_min = 1,
+                ),
+            }
+
+        }, # cat7
+
+    #----------
+    "cat6": {
+    #----------
+        "vbf": {
+            150: dict(
+                sigma0_min = 4,
+                sigma1_min = 4,
+
+                )
+            }
+
+        }, # cat7
+
+
+    #----------
+    "cat7": {
+    #----------
+        "ggh": {
+            115: dict(
+                sigma0_min = 2,
+                sigma1_min = 2,
+
+                )
+            }
+
+        }, # cat7
+
+    #----------
+
+
+    }
