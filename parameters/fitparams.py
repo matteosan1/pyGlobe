3eecf516750fa6449af0d7a7c5ee6fadf9c0a4a3
Andre Holzner <andre.holzner@gmail.com>
2015-02-18 11:17:32 +0100

    added more cases for fit parameters (status as of 2015-01-21 15:40)

--- parameters/fitparams.py
+++ parameters/fitparams.py
@@ -1,6 +1,20 @@
 params = {
 
     #----------
+    "cat2": {
+    #----------
+        "ggh": {
+            130: dict(
+                # sigma0_min = 0.8,
+                # sigma1_min = 0.8,
+                ),
+
+            },
+
+
+        }, # cat2
+
+    #----------
     "cat5": {
     #----------
         "vbf": {
@@ -26,6 +40,15 @@ params = {
     "cat6": {
     #----------
         "vbf": {
+            115: dict(
+                sigma0_min = 1,
+                sigma1_min = 1,
+
+                sigma0_max = 3,
+                sigma1_max = 3,
+
+
+                ),
             150: dict(
                 sigma0_min = 4,
                 sigma1_min = 4,
@@ -44,10 +67,101 @@ params = {
                 sigma0_min = 2,
                 sigma1_min = 2,
 
-                )
-            }
+                ),
+
+            125: dict(
+                sigma0_min = 1,
+                sigma1_min = 1,
+                
+                ),
+
+            140: dict(
+                sigma0_max = 6,
+                sigma1_max = 6,
+                # 
+                sigma0_min = 1,
+                sigma1_min = 1,
+
+                ),
+
+            }, # ggh
+
+    
 
         }, # cat7
+
+    #----------
+    "cat8": {
+    #----------
+        "ggh": {
+            120: dict(
+                sigma0_min = 2,
+                sigma1_min = 2,
+
+                ),
+            }, # ggh
+
+        "vbf": {
+            125: dict(
+                sigma0_max = 8,
+                sigma1_max = 8,
+
+                ),
+
+            130: dict(
+                dmu0_min = -1,
+                dmu1_min = -1,
+
+                sigma0_min = 2,
+                sigma1_min = 2,
+
+                ),
+            }, # vbf
+
+    
+
+        }, # cat8
+
+    #----------
+    "cat9": {
+    #----------
+        "ggh": {
+            115: dict(
+                sigma0_min = 0.5,
+                sigma1_min = 0.5,
+
+                ),
+            }, # ggh
+
+
+        }, # cat9
+
+    #----------
+    "cat10": {
+    #----------
+        "ggh": {
+            115: dict(
+                dmu0_min = -0.8,
+                dmu1_min = -0.8,
+
+                sigma0_min = 0.5,
+                sigma1_min = 0.5,
+
+
+                ),
+            120: dict(
+                # dmu0_min = -0.8,
+                # dmu1_min = -0.8,
+
+                sigma0_min = 0.5,
+                sigma1_min = 0.5,
+
+
+                ),
+            }, # ggh
+
+
+        }, # cat10
 
     #----------
 
