ceefbb17d41dfa99d374f12b7a0e8dddd7b94986
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2015-02-22 21:33:00 +0100

    initial commit: script to run the signal and background model generation (which are several steps) for the workspace

---
+++ makeWorkspaceStep2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# stop script on first error
+set -e
+
+# step 1 would be:
+# fill the MC and data datasets
+# ./pyGlobe.py -i ../data/emu_merged_v8.root -o emu_output_v5.root --wsout workspace-raw-unblinded.root
+
+# build the signal model
+./fitSignal.py --simultaneous workspace-raw-unblinded.root /tmp/ws2.root
+
+# fix the signal model
+./fixSignalModel.py /tmp/ws2.root /tmp/ws3.root 
+
+# build the background model (needs RooPower from CMSSW, so done as last step)
+./fitBackground.py parameters/bgfunc.py /tmp/ws3.root workspace-nominal-nonblind.root
+
