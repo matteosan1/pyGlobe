22b57423eebf4fb6f896a6607c489ba39ee26b58
Andre Holzner <holzner@andres-mbp-2.fritz.box>
2015-02-22 10:13:46 +0100

    selected lower polynomial order for cat0 (within one sigma of lowest bias function), added missing function for cat7

--- parameters/bgfunc.py
+++ parameters/bgfunc.py
@@ -2,13 +2,14 @@ bgfuncs = {
 
     # lowest bias functions as determined 2015-02-14
 
-    "cat0":  "pol7",
+    "cat0":  "pol5",
     "cat1":  "pol4",
     "cat2":  "pow1",
     "cat3":  "pol5",
     "cat4":  "exp1",
     "cat5":  "exp1",
     "cat6":  "pol7",
+    "cat7":  "pol5",
     "cat8":  "pol5",
     "cat9":  "pol1",
     "cat10": "pol2",
