773bd34e81be1ac5e14dc423fb275c23d7a91dae
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2015-03-06 17:42:11 +0100

    initial commit: alternative background function for cat6

---
+++ parameters/bgfunc-cat6mod.py
@@ -0,0 +1,16 @@
+bgfuncs = {
+
+    # lowest bias functions as determined 2015-02-14
+
+    "cat0":  "pol5",
+    "cat1":  "pol4",
+    "cat2":  "pow1",
+    "cat3":  "pol5",
+    "cat4":  "exp1",
+    "cat5":  "exp1",
+    "cat6":  "pol4",
+    "cat7":  "pol5",
+    "cat8":  "pol5",
+    "cat9":  "pol1",
+    "cat10": "pol2",
+    }
