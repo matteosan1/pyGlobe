1188a80fe06da372dbcb02df9f4103c4434a668b
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2015-09-15 23:07:06 +0200

    fixed comment

--- jesJerTest-2015-09-04-makeworkspaces.sh
+++ jesJerTest-2015-09-04-makeworkspaces.sh
@@ -20,7 +20,7 @@ python pyGlobe.py -i ../data/2015-07-01-hemu_jet_syst_jec_up.root   -o emu_outpu
 python pyGlobe.py -i ../data/2015-07-01-hemu_jet_syst_jec_down.root -o emu_output_v5.root --blind --wsout $OUTDIR/jes-down-met-fixed.root  --metfile ../data/2015-07-01-hemu_jet_syst_jec_no.root --eventlist $OUTDIR/jes-down-met-fixed.csv
 
 #----------
-# JER: jet energies and MET shifted
+# JER: jet resolution and MET shifted
 #----------
 python pyGlobe.py -i ../data/2015-07-01-hemu_jet_syst_jer_up.root   -o emu_output_v5.root --blind --wsout $OUTDIR/jer-up.root    --eventlist $OUTDIR/jer-up.csv 
 python pyGlobe.py -i ../data/2015-07-01-hemu_jet_syst_jer_down.root -o emu_output_v5.root --blind --wsout $OUTDIR/jer-down.root  --eventlist $OUTDIR/jer-down.csv 
