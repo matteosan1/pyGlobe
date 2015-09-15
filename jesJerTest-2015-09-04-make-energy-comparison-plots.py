e0dd7ef59a3bfdd26fc58fff8e3e3c082de0960b
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2015-09-15 23:10:46 +0200

    initial commit: script to make jet energy comparison plots

---
+++ jesJerTest-2015-09-04-make-energy-comparison-plots.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys, os
+
+# jet energy scale
+# ../data/2015-07-01-hemu_jet_syst_jec_up.root   $OUTDIR/jes-up.root   
+# ../data/2015-07-01-hemu_jet_syst_jec_down.root $OUTDIR/jes-down.root 
+# ../data/2015-07-01-hemu_jet_syst_jec_no.root   $OUTDIR/nominal.root  
+
+for itype, cats in (
+    [-225, [0,1,2,3,4,5,6,7,8 ]],
+    [-325, [9,10]]):
+
+    for mode in [ 'jec', 'jer']:
+        cmdParts = [
+            "../compareJetEnergies.py",
+            '-o "jet-energies-' + mode + '-cat{cat}-{proc}.png"',
+            "--cats " + ",".join([ str(x) for x in cats]),
+            '--',
+            str(itype),
+            'nominal.csv',
+            '../../data/2015-07-01-hemu_jet_syst_jec_no.root',
+            '../../data/2015-07-01-hemu_jet_syst_%s_up.root' % mode,
+            ]
+
+        cmd = " ".join(cmdParts)
+
+        print "cmd=",cmd
+        res = os.system(cmd)
+
+        assert res == 0
+        
